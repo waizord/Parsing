3d10943cca901d3e5ac40b091ee557c430ca96bf
Artem <jester.b1@mail.ru>
2020-05-28 01:23:54 +0300

    Initial Commit

---
+++ Parsing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parsing
+//
+//  Created by Indoril Nerevar on 5/28/20.
+//  Copyright © 2020 Artem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
